99c2a2a5f685f8e0f3e557d4a5177febde3aa0ba
Nathaniel Ehrlich <nathaniel.ehrlich@parentsquare.com>
2024-05-20 22:21:44 -0600

    fixed statis issue

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,7 +23,7 @@ ROOT_DIR = BASE_DIR.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("PRODUCTION_HOST")
 
