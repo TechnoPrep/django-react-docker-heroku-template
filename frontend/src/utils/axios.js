9ca9e3ba0ae43ee470ffa14ab674b5c2c1d8be66
Nathaniel Ehrlich <nathaniel.ehrlich@parentsquare.com>
2024-05-20 23:45:45 -0600

    hard coded url

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.DEV ? 'http://localhost:8000/api' : 'https://django-react-heroku-test-1de48afcdf56.herokuapp.com/api';
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 });
