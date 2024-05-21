e10d0c57a4a618fa9001650f24e148aedd855bc0
Nathaniel Ehrlich <nathaniel.ehrlich@parentsquare.com>
2024-05-20 21:56:49 -0600

    fixed dir issues

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_DIR = BASE_DIR.parent
 
 # Quick-start development settings - unsuitable for production
