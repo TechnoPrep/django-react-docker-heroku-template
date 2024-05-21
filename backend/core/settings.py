8b3c57a43ac62c75f797e7e3738e7cd2326f6a4e
Nathaniel Ehrlich <nathaniel.ehrlich@parentsquare.com>
2024-05-20 22:35:00 -0600

    I dont fuckin know at this point

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,20 +20,20 @@ ROOT_DIR = BASE_DIR.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY', 'development_without_docker')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', True) 
 
-ALLOWED_HOSTS = os.environ.get("PRODUCTION_HOST")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
 # Application definition
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django_extensions',
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     "contracts",
 ]
 
-INSTALLED_APPS.extend(["whitenoise.runserver_nostatic"])
+if not DEBUG:
+    INSTALLED_APPS.append("whitenoise.runserver_nostatic")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -56,98 +57,84 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
-ROOT_URLCONF = "Ledgifier.urls"
+if not DEBUG:
+    MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(ROOT_DIR, "frontend", "build")],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(ROOT_DIR, "frontend", "build")] if not DEBUG else [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "Ledgifier.wsgi.application"
+WSGI_APPLICATION = 'core.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# Add these at the top of your settings.py
+# Database configuration
 DATABASE_URL = os.environ.get('DATABASE_URL')
-
-# Replace the DATABASES section of your settings.py with this
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL)
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=False)
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(ROOT_DIR, "frontend", "build", "assets")]
-
-# type of static files storage
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-# directory to which Django will move those static assets and from which it will serve them when the app is running
-STATIC_ROOT = os.path.join(ROOT_DIR, "staticfiles")
-
-STATIC_URL = "/static/"
-
-# directory where WhiteNoise can find all non-html static assets
-WHITENOISE_ROOT = os.path.join(ROOT_DIR, "frontend", "build")
+if not DEBUG:
+    STATICFILES_DIRS = [os.path.join(ROOT_DIR, "frontend", "build", "assets")]
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATIC_ROOT = os.path.join(ROOT_DIR, "staticfiles")
+    WHITENOISE_ROOT = os.path.join(ROOT_DIR, "frontend", "build")
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-# ]
+# CORS headers configuration
+CORS_ALLOW_ALL_ORIGINS = DEBUG
+
+# required by django.contrib.sites
+SITE_ID = 1
 
 APPEND_SLASH = False
 AUTH_USER_MODEL = "users.User"
+
+# Django REST Framework config
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        ('rest_framework.permissions.AllowAny',)
+    ),
+}
